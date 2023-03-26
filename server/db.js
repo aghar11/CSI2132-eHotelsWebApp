897466fcf935e4e11f0d6cab29ab4aad05868a18
Akarsh Gharge <akarshgharge20@gmail.com>
2023-03-26 12:54:10 -0400

    Added database connection file

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("ps").Pool;
+
+const pool = new Pool({
+    user: "postgres",
+    password: "admin123",
+    host: "localhost",
+    port: 5432,
+    database: "ehotelsDB"
+});
+
+module.exports = pool;
