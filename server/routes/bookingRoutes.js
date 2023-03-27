79ca5fb3520a062ca7cbba7efd63a61638f9128b
Akarsh Gharge <akarshgharge20@gmail.com>
2023-03-27 13:52:47 -0400

    Added route organization

---
+++ server/routes/bookingRoutes.js
@@ -0,0 +1,23 @@
+/**
+ * Handle API requests related to the Booking table.
+ *
+ * @author 
+ * @since  March, 2023
+ */
+
+// Setup
+const express = require('express');
+const pool = require("../db");
+const bodyParser = require("body-parser");
+const router = express.Router();
+
+/**
+ * Booking API Routes
+ */
+
+
+
+
+
+
+module.exports = router;
