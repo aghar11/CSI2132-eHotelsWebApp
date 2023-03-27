6f5a85e305f9dbb4087166779d4aeb37ce370f26
Akarsh Gharge <akarshgharge20@gmail.com>
2023-03-27 14:28:42 -0400

    Added console.log statements to hotelRoutes.js

--- server/routes/hotelRoutes.js
+++ server/routes/hotelRoutes.js
@@ -30,20 +30,20 @@ const router = express.Router();
  *   }
  */
 router.post("/hotel", async(req, res)=> {
-
     try {
         const hotel = req.body;
+        console.log("Adding Hotel: "+JSON.stringify(hotel)+" to database.");
+
         const newHotel = await pool.query(
-            "INSERT INTO Hotel (HotelID, CompanyName, Address, Category, NumberOfRooms) VALUES ($1, $2, $3, $4, $5) RETURNING *;", [hotel.hotelID, 
-            hotel.companyName, hotel.address, hotel.category, hotel.numberOfRooms]
+            "INSERT INTO Hotel (HotelID, CompanyName, Address, Category, NumberOfRooms) VALUES ($1, $2, $3, $4, $5) RETURNING *", [hotel.hotelID, 
+                hotel.companyName, hotel.address, hotel.category, hotel.numberOfRooms]
         );
 
-        res.json(newHotel.rows[0]);
+        res.json(newHotel.rows);
 
     } catch (err) {
         console.error(err.message);
     }
-    
 });
 
 /**
@@ -55,8 +55,9 @@ router.post("/hotel", async(req, res)=> {
  *   { }
  */
 router.get("/hotel", async(req, res)=>{
-
     try {
+        console.debug("Retriving all hotels from database.");
+
         const allHotels = await pool.query("Select * from Hotel");
         res.json(allHotels.rows);
 
@@ -78,6 +79,8 @@ router.get("/hotel/:id", async(req, res)=>{
 
     try {
         const {id} = req.params;
+        console.debug("Retriving Hotel with ID:"+id+" from database.")
+
         const hotel = await pool.query("SELECT * FROM Hotel WHERE HotelID = $1", [id])
         res.json(hotel.rows[0]);
 
@@ -101,6 +104,8 @@ router.put("/hotel/category/:id", async(req, res)=>{
     try {
         const {id} = req.params;
         const {category} = req.body.category;
+        console.debug("Updating Hotel category of hotel with ID:"+id+" to "+category+".");
+
         const updateHotel = await pool.query("UPDATE Hotel SET category = $1 WHERE HotelID = $2", [category, id]);
         res.json("Hotel was updated!");
 
@@ -122,6 +127,8 @@ router.delete("/hotel/:id", async(req, res)=>{
 
     try {
         const {id} = req.params;
+        console.log("Deleting Hotel with ID:"+id+".")
+
         const deleteHotel = await pool.query("DELETE FROM Hotel WHERE HotelID = $1", [id]);
         res.json("Hotel was deleted!");
 
