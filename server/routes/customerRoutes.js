7ea42ed7dcfdad5652caa25310e70ac0aaa94228
AxelTWC <axel0328@outlook.com>
2023-03-28 15:16:08 -0400

    Only Post function

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -14,7 +14,25 @@ const router = express.Router();
 /**
  * Customer API Routes
  */
+router.post("/customers", async(req, res) => {
+    try {
+        const customer = req.body;
+        const address = req.body.address;
+        const name = req.body.name;
 
+        console.debug("Adding Customer: "+JSON.stringify(customer)+" to database.")
+
+        const newCustomer =  await pool.query(
+            "INSERT INTO Customer (CustomerID, SIN, RegistrationDate, FirstName, LastName, StreetNumber, StreetName, AptNumber, City, State, PostalCode) VALUES \
+            ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *",
+            [customer.customerID, customer.SIN , customer.registrationDate, name.firstName, name.lastName, address.streetNumber, address.streetName, address.aptNumber, address.city, address.state, address.postalCode]
+        );
+
+        res.json(newCustomer.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 
 
