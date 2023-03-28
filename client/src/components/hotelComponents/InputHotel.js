b99421c49361e3939f330c0fd15d70ea01258aaa
aghar11 <60454878+aghar11@users.noreply.github.com>
2023-03-27 21:29:27 -0400

    Fixed create hotel request body attribute labels (#7)
    * Frontend package organization (#4)
    
    * Fixed create hotel API body attribute labels

--- client/src/components/hotelComponents/InputHotel.js
+++ client/src/components/hotelComponents/InputHotel.js
@@ -2,16 +2,16 @@ import React, {Fragment, useState} from "react";
 
 const InputHotel = ()=> {
 
-    const [hotelId , sethotelId] = useState("HotelID")
+    const [hotelID , sethotelID] = useState("HotelID")
     const [category , setcategory] = useState("Category")
-    const [companyname , setcompanyname] = useState("Company Name")
+    const [companyName , setcompanyName] = useState("Company Name")
     const [address , setaddress] = useState("Address")
-    const [numberofrooms , setnumberofrooms] = useState("Number of rooms")
+    const [numberOfRooms , setnumberOfRooms] = useState("Number of rooms")
 
     const onSubmitForm = async e => {
         e.preventDefault();
         try {
-            const body = {hotelId, companyname, address, category, numberofrooms};
+            const body = {hotelID, companyName, address, category, numberOfRooms};
             const response = await fetch("http://localhost:5000/api/hotel", {
                 method: "POST",
                 headers: {"Content-Type": "application/json"},
@@ -26,11 +26,11 @@ const InputHotel = ()=> {
         <Fragment>
             <h1 className = "text-centre mt-5"> Input Hotel</h1>
             <form className = "d-flex mt-5" onSubmit={onSubmitForm}>
-                <input type= "text" className="form-control" value = {hotelId} onChange={e => sethotelId(e.target.value)}></input>
-                <input type= "text" className="form-control" value = {companyname} onChange={e => setcompanyname(e.target.value)}></input>
+                <input type= "text" className="form-control" value = {hotelID} onChange={e => sethotelID(e.target.value)}></input>
+                <input type= "text" className="form-control" value = {companyName} onChange={e => setcompanyName(e.target.value)}></input>
                 <input type= "text" className="form-control" value = {address} onChange={e => setaddress(e.target.value)}></input>
                 <input type= "text" className="form-control" value = {category} onChange={e => setcategory(e.target.value)}></input>
-                <input type= "text" className="form-control" value = {numberofrooms} onChange={e => setnumberofrooms(e.target.value)}></input>
+                <input type= "text" className="form-control" value = {numberOfRooms} onChange={e => setnumberOfRooms(e.target.value)}></input>
                 <button className= "btn btn-success">Add</button>
             </form>
         </Fragment>
