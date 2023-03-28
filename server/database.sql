32ff49ede31192c4dca45f4df650a16c2af666b7
Akarsh Gharge <akarshgharge20@gmail.com>
2023-03-27 22:29:17 -0400

    Updated database setup SQL Script

--- server/database.sql
+++ server/database.sql
@@ -4,29 +4,39 @@ CREATE DATABASE postgres;
 
 
 CREATE TABLE IF NOT EXISTS Headquarters(
-	CompanyName Varchar(20) PRIMARY KEY,
-	NumberOfHotels Integer,
-	Address Varchar(20)
+	CompanyName Varchar(225) PRIMARY KEY,
+	NumberOfHotels Integer NOT NULL,
+	StreetNumber Integer NOT NULL,
+	StreetName Varchar(225) NOT NULL,
+	AptNumber Varchar(25),
+	City Varchar(225) NOT NULL,
+	State Varchar(225) NOT NULL,
+	PostalCode Varchar(25) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS HeadquartersPhone(
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	phoneNumber VarChar(20) PRIMARY KEY,
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters
 );
 
 CREATE TABLE IF NOT EXISTS HeadquartersEmail(
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	email VarChar(40) PRIMARY KEY,
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters
 );
 
 CREATE TABLE IF NOT EXISTS Hotel (
 	HotelID Integer PRIMARY KEY,
-	CompanyName Varchar(20),
-	Address Varchar(20),
-	Category Varchar(20),
-	NumberOfRooms Integer,
+	CompanyName Varchar(225) NOT NULL,
+	Category Varchar(225) NOT NULL,
+	NumberOfRooms Integer NOT NULL,
+	StreetNumber Integer NOT NULL,
+	StreetName Varchar(225) NOT NULL,
+	AptNumber Varchar(25),
+	City Varchar(225) NOT NULL,
+	State Varchar(225) NOT NULL,
+	PostalCode Varchar(25) NOT NULL,
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters
 );
 
@@ -44,12 +54,12 @@ CREATE TABLE IF NOT EXISTS HotelEmail(
 
 CREATE TABLE IF NOT EXISTS Room (
 	RoomNumber Integer PRIMARY KEY,
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	HotelID Integer,
-	ViewType Varchar(20),
-	Price Integer,
-	Capacity Integer,
-	Expandable Varchar(20),
+	ViewType Varchar(225) NOT NULL,
+	Price Integer NOT NULL,
+	Capacity Integer NOT NULL,
+	Expandable Varchar(225) NOT NULL,
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters,
 	FOREIGN KEY (HotelID) REFERENCES Hotel
 );
@@ -58,7 +68,7 @@ CREATE TABLE IF NOT EXISTS RoomIssue(
 	Issue Varchar(20) PRIMARY KEY,
 	RoomNumber Integer,
 	HotelID Integer,
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters,
 	FOREIGN KEY (HotelID) REFERENCES Hotel,
 	FOREIGN KEY (RoomNumber) REFERENCES Room
@@ -68,7 +78,7 @@ CREATE TABLE IF NOT EXISTS RoomAmenity(
 	Amenity Varchar(20) PRIMARY KEY,
 	RoomNumber Integer,
 	HotelID Integer,
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters,
 	FOREIGN KEY (HotelID) REFERENCES Hotel,
 	FOREIGN KEY (RoomNumber) REFERENCES Room
@@ -76,34 +86,46 @@ CREATE TABLE IF NOT EXISTS RoomAmenity(
 
 CREATE TABLE IF NOT EXISTS Customer (
 	CustomerID Integer PRIMARY KEY,
-	SIN Varchar(20),
-	RegistrationDate Date,
-	FirstName Varchar(20),
-	LastName Varchar(20),
-	Address Varchar(20)
+	SIN Varchar(20) NOT NULL,
+	RegistrationDate Date NOT NULL,
+	FirstName Varchar(20) NOT NULL,
+	LastName Varchar(20) NOT NULL,
+	StreetNumber Integer NOT NULL,
+	StreetName Varchar(225) NOT NULL,
+	AptNumber Varchar(25),
+	City Varchar(225) NOT NULL,
+	State Varchar(225) NOT NULL,
+	PostalCode Varchar(25) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Booking (
-	CheckInDate Date PRIMARY KEY,
+	CheckInDate Date,
 	CheckOutDate Date,
 	HotelID Integer,
 	RoomNumber Integer,
 	CustomerID Integer,
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters,
 	FOREIGN KEY (HotelID) REFERENCES Hotel,
 	FOREIGN KEY (CustomerID) REFERENCES Customer,
-	FOREIGN KEY (RoomNumber) REFERENCES Room
+	FOREIGN KEY (RoomNumber) REFERENCES Room,
+	PRIMARY KEY (CheckInDate, CheckOutDate)
 );
 
 CREATE TABLE IF NOT EXISTS Employee (
 	EmployeeID Integer PRIMARY KEY,
-	SIN Varchar(20),
+	SIN Varchar(20) NOT NULL,
 	HotelID Integer,
-	CompanyName Varchar(20),
-	FirstName Varchar(20),
-	LastName Varchar(20),
-	Address Varchar(20),
+	CompanyName Varchar(225),
+	FirstName Varchar(20) NOT NULL,
+	MiddleName Varchar(20),
+	LastName Varchar(20) NOT NULL,
+	StreetNumber Integer NOT NULL,
+	StreetName Varchar(225) NOT NULL,
+	AptNumber Varchar(25),
+	City Varchar(225) NOT NULL,
+	State Varchar(225) NOT NULL,
+	PostalCode Varchar(25) NOT NULL,
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters,
 	FOREIGN KEY (HotelID) REFERENCES Hotel
 );
@@ -112,7 +134,7 @@ CREATE TABLE IF NOT EXISTS Employee (
 CREATE TABLE IF NOT EXISTS Manages (
 	EmployeeID Integer,
 	HotelID Integer,
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	PRIMARY KEY (EmployeeID, HotelID, CompanyName),
 	FOREIGN KEY (EmployeeID) REFERENCES Employee,
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters,
@@ -122,7 +144,7 @@ CREATE TABLE IF NOT EXISTS Manages (
 CREATE TABLE IF NOT EXISTS EmployeeRole (
 	EmployeeID Integer,
 	HotelID Integer,
-	CompanyName Varchar(20),
+	CompanyName Varchar(225),
 	Role Varchar(20) PRIMARY KEY,
 	FOREIGN KEY (EmployeeID) REFERENCES Employee,
 	FOREIGN KEY (CompanyName) REFERENCES Headquarters,
