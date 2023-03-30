a302612f3c2862ae47fd5df0426b9efb597103ee
AxelTWC <axel0328@outlook.com>
2023-03-30 15:42:58 -0400

    Temporary

--- server/db.js
+++ server/db.js
@@ -10,7 +10,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "Arsenal14",
+    password: "Axel1234",
     host: "localhost",
     port: 5432,
     database: "postgres"
