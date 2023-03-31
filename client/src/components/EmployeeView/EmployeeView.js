af739f5577eb30aa5e3e2d8268f80e5bcd7af475
Akarsh Gharge <akarshgharge20@gmail.com>
2023-03-31 13:59:50 -0400

    Added missing import

--- client/src/components/EmployeeView/EmployeeView.js
+++ client/src/components/EmployeeView/EmployeeView.js
@@ -1,6 +1,7 @@
 import React, {Fragment, useState} from "react"
 import { Link } from 'react-router-dom'
 import ListHotels from "./hotelComponents/ListHotels";
+import ListHeadquarter from "./headquarterComponents/ListHeadquarter"
 
 
 const EmployeeView = () => {
