351c2e64402360d6aa7a972d4e0531473dedb041
Akarsh Gharge <akarshgharge20@gmail.com>
2023-03-31 09:33:58 -0400

    CustomerDashboard clean up

--- client/src/components/CustomerView/CustomerDashboard.js
+++ client/src/components/CustomerView/CustomerDashboard.js
@@ -1,6 +1,6 @@
 import '../../App.css';
 import React, { useEffect, useState} from "react";
-import { json, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 
 function CustomerDashboard() {
